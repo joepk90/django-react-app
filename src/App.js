9174c88f31543d6ad05d06b6107402b9924e0588
Joe <joeparkkennaby@gmail.com>
2022-01-29 20:14:08 +0000

    setup basic form in app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import Container from './components/common/container';
+import Section from './components/common/section';
 import logo from './logo.svg';
 import './App.css';
+import Form from "@rjsf/core";
 
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Django React App - Form Example
       </header>
+
+      <Section>
+        <Container>
+          <Form
+            schema={{}}
+            uiSchema={{}}
+            formData={{}}
+          // widgets={widgets}
+          />
+        </Container>
+      </Section>
     </div>
   );
 }
