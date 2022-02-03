0d69a46b2a6e481c2507d0db6fce731057d94348
Joe <joeparkkennaby@gmail.com>
2022-02-03 19:25:41 +0000

    convert django form setup into DjangoForm class component

--- src/App.js
+++ src/App.js
@@ -1,78 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import DjangoForm from './components/DjangoForm';
 import Container from './components/common/container';
 import Section from './components/common/section';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
-import Form from "@rjsf/core";
-
-// .env: REACT_APP_API_URL=http://domain.com
-const HOST = process.env.REACT_APP_API_URL
-
-const handleFormSubmit = async (event) => {
-
-  if (!event || !event.formData) {
-    toast.error("Something went wrong...");
-  }
-
-  const response = await fetch(HOST + "/blog/posts/1/",
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "PUT",
-      body: JSON.stringify(event.formData)
-    })
-    .then((result) => result.json())
-    .then((result) => {
-      toast.error(result.non_field_errors[0]);
-    });
-
-  const { status } = response;
-
-  if (status && status === 200) {
-    toast.success("Success!");
-  } else {
-    toast.error("Something went wrong...");
-  }
-
-}
-
-const renderForm = (serializer, formData) => {
-
-  if (!serializer || !formData) return
-
-  return (
-    <Section>
-      <Container>
-        <Form
-          schema={serializer.schema}
-          uiSchema={serializer.uiSchema}
-          formData={formData}
-          onSubmit={handleFormSubmit}
-        />
-      </Container>
-    </Section>
-  )
-}
 
 function App() {
-
-  const [serializer, setSerializer] = useState(null);
-  const [formData, setFormData] = useState(null);
-
-  useEffect(() => {
-    fetch(HOST + '/blog/forms/1/')
-      .then(results => results.json())
-      .then(data => {
-
-        const { formData, serializer } = data;
-
-        setSerializer(serializer);
-        setFormData(formData);
-      });
-  }, [])
 
   return (
     <div className="App">
@@ -85,7 +18,7 @@ function App() {
         </Section>
       </header>
 
-      {renderForm(serializer, formData)}
+      <DjangoForm />
 
     </div>
   );
---
+++ src/components/DjangoForm.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Container from './common/container';
+import Section from './common/section';
+import Form from "@rjsf/core";
+
+// .env: REACT_APP_API_URL=http://domain.com
+const HOST = process.env.REACT_APP_API_URL
+
+class DjangoForm extends Component {
+    state = {
+        schema: {},
+        uiSchema: {},
+        formData: {},
+    }
+
+    componentDidMount = () => {
+
+        (async () => {
+
+            fetch(HOST + '/blog/forms/1/')
+                .then(results => results.json())
+                .then(data => {
+
+                    const { formData, serializer } = data;
+
+                    this.setState({
+                        schema: serializer.schema,
+                        uiSchema: serializer.uiSchema,
+                        formData: formData,
+                    })
+                });
+
+        })()
+
+
+    }
+
+    handleFormSubmit = async (event) => {
+
+        if (!event || !event.formData) {
+            toast.error("Something went wrong...");
+        }
+
+        const response = await fetch(HOST + "/blog/posts/1/",
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "PUT",
+                body: JSON.stringify(event.formData)
+            })
+            .then((result) => result.json())
+            .then((result) => {
+                toast.error(result.non_field_errors[0]);
+            });
+
+        const { status } = response;
+
+        if (status && status === 200) {
+            toast.success("Success!");
+        } else {
+            toast.error("Something went wrong...");
+        }
+
+        return response;
+
+    }
+
+    render() {
+
+        const { schema, uiSchema, formData } = this.state;
+
+        if (!schema || !uiSchema || !formData) return
+
+        return (
+            <Section>
+                <Container>
+                    <Form
+                        schema={schema}
+                        uiSchema={uiSchema}
+                        formData={formData}
+                        onSubmit={this.handleFormSubmit}
+                    />
+                </Container>
+            </Section>
+        )
+    }
+}
+
+export default DjangoForm;
