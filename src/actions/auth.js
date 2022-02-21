525cb17de9ea7296d0b3c034d76d8c4761d42ec2
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-02-21 22:23:56 +0000

    setup new actions and reducers in auth redux store: AUTHENTICATED_SUCCESS, AUTHENTICATED_FAIL, LOGOUT

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,11 +4,48 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     USER_LOADED_SUCCESS,
-    USER_LOADED_FAIL
+    USER_LOADED_FAIL,
+    AUTHENTICATED_SUCCESS,
+    AUTHENTICATED_FAIL,
+    LOGOUT
 } from './types';
 
 // .env: REACT_APP_API_URL=http://domain.com
 const HOST = process.env.REACT_APP_API_URL
+
+export const checkAuthenticated = () => async dispatch => {
+    if (localStorage.getItem('access')) {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${localStorage.getItem('access')}`,
+                'Accept': 'application/json'
+            }
+        };
+
+        const body = JSON.stringify({
+            token: localStorage.getItem('access')
+        })
+
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/verify`, body, config);
+
+            dispatch({
+                type: AUTHENTICATED_SUCCESS,
+            })
+
+        } catch (err) {
+            dispatch({
+                type: AUTHENTICATED_FAIL,
+            })
+        }
+
+    } else {
+        dispatch({
+            type: AUTHENTICATED_FAIL,
+        })
+    }
+}
 
 export const load_user = () => async dispatch => {
     if (localStorage.getItem('access')) {
@@ -69,4 +106,10 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL,
         })
     }
+}
+
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT,
+    })
 }
