8d48593e7e7fb0a1804dd9fc5422fd48a8887a5c
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-02-21 22:27:05 +0000

    update the navigation component to render a login or logout button depending isAuthentication state property. setup logic to logout the user to direct the user to the login page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { logout } from '../actions/auth'
 
-const Navigation = () => (
+const Navigation = ({ logout, isAuthenticated }) => {
 
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">Home</Link>
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            {/* <li class="nav-item active">
-                <Link class="nav-link" to="/about">About</Link>
-            </li> */}
-        </ul>
-        <Link className="btn btn-info my-2 my-sm-0" to="/login">Login</Link>
-    </nav>
-)
+    const authLinks = [
+        {
+            id: 'logout',
+            label: 'Logout',
+            path: '/',
+            event: () => logout
+        }
+    ]
 
-export default Navigation;
+    const guestLinks = [
+        {
+            id: 'login',
+            label: 'Login',
+            path: '/login'
+        }
+    ]
+
+    const links = isAuthenticated === true ? authLinks : guestLinks
+
+    const renderLinks = (links) => {
+        return links.map(link => {
+            return <Link
+                key={link.path}
+                className="btn btn-info my-2 my-sm-0"
+                onClick={logout}
+                to={link.path}
+            >{link.label}</Link>
+        })
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                {/* <li class="nav-item active">
+            <Link class="nav-link" to="/about">About</Link>
+        </li> */}
+            </ul>
+            {renderLinks(links)}
+        </nav>
+    )
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { logout })(Navigation);
