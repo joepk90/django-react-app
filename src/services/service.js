144c9a373c51f3bf3ec0a3e46136d62bdaeaad00
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-02-26 22:05:41 +0000

    setup base service file

---
+++ src/services/service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+// .env: REACT_APP_API_URL=http://domain.com
+const HOST = process.env.REACT_APP_API_URL
+
+axios.defaults.baseURL = HOST;
+
+axios.interceptors.response.use(null, error => {
+
+    const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+
+    if (!expectedError) {
+        toast.error('unexpected error occured');
+    }
+
+    return Promise.reject(error);
+
+});
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete,
+}
+
+
